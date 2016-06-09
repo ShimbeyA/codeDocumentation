386c2fbbcddc8372a5c8222e5bcbcf1f8aa54be6
ShimbeyA <shimbeyassie@yahoo.com>
2016-06-09 14:58:16 -0700

    code documentation

---
+++ captcha.php
@@ -0,0 +1,26 @@
+
+<?php
+	// this is to create a captcha that will spam out data entered
+	
+    foreach ($_POST as $key => $value){
+        echo '<p><strong>' . $key.':</strong>' .$value .'</p>';
+    }
+    
+    // your secret key
+$secret = "6LcePAATAAAAABjXaTsy7gwcbnbaF5XgJKwjSNwT";
+ 
+// empty response
+$response = null;
+ 
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
+
+// if submitted check response
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+
+?>
